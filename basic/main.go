c866179d70134f1dca86ad0c58d24fe91e8f9c2c
aleksandarss <aleksandar-sasa.calic@hotmail.com>
2026-03-21 00:59:40 +0100

    feat: wire up the new flag with clientcmd

--- basic/main.go
+++ basic/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type podsOptions struct {
@@ -37,12 +38,26 @@ func newPodsCmd() *cobra.Command {
 			fmt.Printf("Listing pods in namespace: %s\n", opts.namespace)
 			fmt.Printf("Output format: %s\n", opts.output)
 			fmt.Printf("Using kubeconfig: %s\n", opts.kubeconfig)
+
+			loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+			loadingRules.ExplicitPath = opts.kubeconfig
+
+			configOverrides := &clientcmd.ConfigOverrides{}
+			kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+
+			restConfig, err := kubeConfig.ClientConfig()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error loading kubeconfig: %v\n", err)
+				os.Exit(1)
+			}
+
+			fmt.Printf("Succesfully loaded kubeconfig, host: %s\n", restConfig.Host)
 		},
 	}
 
 	cmd.Flags().StringVarP(&opts.namespace, "namespace", "n", "default", "Kubernetes namespace")
 	cmd.Flags().StringVarP(&opts.output, "output", "o", "table", "Output format (table|json|yaml)")
-	cmd.Flags().StringVarP(&opts.kubeconfig, "kubeconfig", "k", "default", "Kubeconfig")
+	cmd.Flags().StringVar(&opts.kubeconfig, "kubeconfig", "", "Path to kubeconfig file (default: $KUBECONFIG or ~/.kube/config)")
 
 	return cmd
 }
